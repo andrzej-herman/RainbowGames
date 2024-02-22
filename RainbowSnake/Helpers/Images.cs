2cab50073a1f734d612d934ea1ff25cd1e5a8b9b
andrzej-herman <aherman@san.edu.pl>
2024-02-22 09:48:31 +0100

    Add RainbowSnakeProject

---
+++ RainbowSnake/Helpers/Images.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace RainbowSnake.Helpers;
+
+public static class Images
+{
+	public static readonly ImageSource Empty = LoadImage("Empty.png");
+	public static readonly ImageSource Food = LoadImage("Food.png");
+	public static readonly ImageSource Body = LoadImage("Body.png");
+	public static readonly ImageSource Head = LoadImage("Head.png");
+	public static readonly ImageSource Tail = LoadImage("Tail.png");
+	public static readonly ImageSource DeadBody = LoadImage("DeadBody.png");
+	public static readonly ImageSource DeadHead = LoadImage("DeadHead.png");
+	public static readonly ImageSource Close = LoadImage("close.png");
+	public static readonly ImageSource CloseHover = LoadImage("close_h.png");
+	public static readonly ImageSource Min = LoadImage("min.png");
+	public static readonly ImageSource MinHover = LoadImage("min_h.png");
+	
+	[SuppressMessage("Performance", "CA1859:Używaj konkretnych typów, aby zwiększyć wydajność – gdy jest to możliwe")]
+	private static ImageSource LoadImage(string fileName)
+	{
+		return new BitmapImage(new Uri($"Assets/{fileName}", UriKind.Relative));
+	}
+}
---
+++ RainbowSnake/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using RainbowSnake.Helpers;
+
+
+namespace RainbowSnake
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void MinButton_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            MinButton.Source = Images.MinHover;
+            MinButton.Width = 38;
+            MinButton.Height = 27;
+        }
+
+        private void MinButton_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MinButton_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            MinButton.Source = Images.Min;
+            MinButton.Width = 38;
+            MinButton.Height = 27;
+        }
+
+        private void CloseButton_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            CloseButton.Source = Images.CloseHover;
+            CloseButton.Width = 38;
+            CloseButton.Height = 27;
+        }
+
+        private void CloseButton_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseButton_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            CloseButton.Source = Images.Close;
+            CloseButton.Width = 38;
+            CloseButton.Height = 27;
+        }
+    }
+}
